df19df6d2fe0355c2b6c141c21f41e1a845724fe
Rodrigo Braz <braz.rodrigo94@gmail.com>
2022-04-05 17:37:54 -0300

    Project created

---
+++ src/main/kotlin/br/com/rodrigobraz/OrderSystemKotlin/OrderSystemKotlinApplication.kt
@@ -0,0 +1,11 @@
+package br.com.rodrigobraz.OrderSystemKotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrderSystemKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<OrderSystemKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/rodrigobraz/OrderSystemKotlin/OrderSystemKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.rodrigobraz.OrderSystemKotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OrderSystemKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
